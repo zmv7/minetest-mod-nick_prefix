e0ac55e29ff95e70a531ffe51783beb1d4d9a93d
zmv7 <zemtzov7@yandex.ru>
2023-11-04 01:27:23 +0500

    Add /hidenick and /shownick

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Provides colored nickname prefixes for chat and nametags
 * `/getprefix [playername]` - check your prefix or prefix of any player
 * `/setprefix <playername> <prefix> <color>` - priv needed: `nick_prefix` - set prefix of any online or offline player
 * `/delprefix [playername]` - delete your prefix or prefix of any player
+* `/hidenick [playername]` - hide your or other player's nametag
+* `/shownick [playername]` - show your or other player's nametag
 * `/players` - view list of online players with their prefixs
--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 nick_prefix = {}
-local s = core.get_mod_storage()
+local hidden_nicks = {}
+local s = minetest.get_mod_storage()
 
 function nick_prefix.get(name)
 	if not name then return end
@@ -17,27 +18,27 @@ function nick_prefix.del(name)
 	nick_prefix.update_ntag(name)
 end
 function nick_prefix.update_ntag(name)
-	local player = core.get_player_by_name(name)
+	local player = minetest.get_player_by_name(name)
 	if player then
 		local vanished = vanish and vanish.vanished[name]
-		if vanished then
+		if vanished or hidden_nicks[name] then
 			player:set_nametag_attributes({color={a=0},text = " "})
 			return
 		end
 		local prefix,color = nick_prefix.get(name)
 		if (prefix and color) then
-			player:set_nametag_attributes({color = {a=255,r=255,g=255,b=255}, text = core.colorize(color,prefix).." "..name})
+			player:set_nametag_attributes({color = {a=255,r=255,g=255,b=255}, text = minetest.colorize(color,prefix).." "..name})
 		else
 			player:set_nametag_attributes({color = {a=255,r=255,g=255,b=255}, text = name})
 		end
 	end
 end
 local timer = 0
-core.register_globalstep(function(dtime)
+minetest.register_globalstep(function(dtime)
 	timer = timer + dtime
 	if timer < 0.3 then return end -- 0.3 sec delay for lower CPU usage
 	timer = 0
-	for _,player in ipairs(core.get_connected_players()) do
+	for _,player in ipairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
 		local ctrl = player:get_player_control()
 		local ntag = player:get_nametag_attributes()
@@ -50,18 +51,18 @@ core.register_globalstep(function(dtime)
 	end
 end)
 
-core.register_privilege("nick_prefix",{description = "Allows to manage nick prefixes", give_to_singleplayer = false})
+minetest.register_privilege("nick_prefix",{description = "Allows to manage nick prefixes", give_to_singleplayer = false})
 
-core.register_on_chat_message(function(name,message)
+minetest.register_on_chat_message(function(name,message)
 	local prefix,color = nick_prefix.get(name)
-	if prefix and color and core.check_player_privs(name, {shout = true}) then
-		core.log("action","CHAT: "..core.format_chat_message(name,core.strip_colors(message)))
-		core.chat_send_all(core.format_chat_message(core.colorize(color,prefix).." "..name,message))
+	if prefix and color and minetest.check_player_privs(name, {shout = true}) then
+		minetest.log("action","CHAT: "..minetest.format_chat_message(name,minetest.strip_colors(message)))
+		minetest.chat_send_all(minetest.format_chat_message(minetest.colorize(color,prefix).." "..name,message))
 		return true
 	end
 end)
 
-core.register_chatcommand("getprefix", {
+minetest.register_chatcommand("getprefix", {
  description = "Get any player's prefix or your own",
  params = "[playername]",
  func = function(name,param)
@@ -70,13 +71,13 @@ core.register_chatcommand("getprefix", {
 	end
 	local prefix,color = nick_prefix.get(param)
 	if prefix and color then
-		return true, param.."'s prefix is "..core.colorize(color,prefix).." (colorstring: "..color..")"
+		return true, param.."'s prefix is "..minetest.colorize(color,prefix).." (colorstring: "..color..")"
 	else
 		return false, "Specified player doesn't have any prefix"
 	end
 end})
 
-core.register_chatcommand("setprefix", {
+minetest.register_chatcommand("setprefix", {
  privs = {nick_prefix=true},
  description = "Set prefix of player",
  params = "<playername> <prefix> <color>",
@@ -84,10 +85,10 @@ core.register_chatcommand("setprefix", {
 	local pname, prefix, color = param:match("(%S+)%s+(%S+)%s+(%S+)")
 	if not (pname and prefix and color) then return false, "Invalid parameters" end
 	nick_prefix.set(pname,prefix,color)
-	return true,"Prefix of "..pname.." now set to "..core.colorize(color,"["..prefix.."]")
+	return true,"Prefix of "..pname.." now set to "..minetest.colorize(color,"["..prefix.."]")
 end})
 
-core.register_chatcommand("delprefix", {
+minetest.register_chatcommand("delprefix", {
  privs = {nick_prefix=true},
  description = "Delete any player's prefix or your own",
  params = "[playername]",
@@ -99,16 +100,46 @@ core.register_chatcommand("delprefix", {
 	return true,"Prefix of "..param.." deleted"
 end})
 
-core.register_chatcommand("prefixes", {
+minetest.register_chatcommand("hidenick",{
+  description = "Hide your or other player's nametag",
+  privs = {nick_prefix=true},
+  params = "[playername]",
+  func = function(name, param)
+	local pname = not param or param == "" and name or param
+	local player = minetest.get_player_by_name(pname)
+	if not player then
+		return false, "Invalid player '"..pname.."'"
+	end
+	hidden_nicks[pname] = true
+	nick_prefix.update_ntag(pname)
+	return true, "Nickname of "..pname.." hidden"
+end})
+
+minetest.register_chatcommand("shownick",{
+  description = "Show hidden nametag",
+  privs = {nick_prefix=true},
+  params = "[playername]",
+  func = function(name, param)
+	local pname = not param or param == "" and name or param
+	local player = minetest.get_player_by_name(pname)
+	if not player then
+		return false, "Invalid player '"..pname.."'"
+	end
+	hidden_nicks[pname] = nil
+	nick_prefix.update_ntag(pname)
+	return true, "Nickname of "..pname.." shown"
+end})
+
+minetest.register_chatcommand("prefixes", {
  privs={nick_prefix=true},
  description="List all players with nick prefixes",
  func = function(name,param)
-	core.chat_send_player(name, "-!- List of players with nick prefixes:")
+	minetest.chat_send_player(name, "-!- List of players with nick prefixes:")
 	local num = 1
 	for player,_ in pairs(s:to_table().fields) do
 		local prefix, color = nick_prefix.get(player)
 		if prefix and color then
-			core.chat_send_player(name, num..") "..player..": "..core.colorize(color,prefix).." ("..color..")")
+			minetest.chat_send_player(name, num..") "..player..": "..minetest.colorize(color,prefix).." ("..color..")")
 			num = num + 1
 		end
 	end
@@ -116,23 +147,23 @@ core.register_chatcommand("prefixes", {
 end})
 
 
-core.register_on_joinplayer(function(player)
+minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
 	if not name then return end
 	nick_prefix.update_ntag(name)
 end)
 
-core.register_chatcommand("players", {
+minetest.register_chatcommand("players", {
  description = "List all players currently online with their nick prefixes",
  func = function(name, param)
-	local list = core.get_connected_players()
+	local list = minetest.get_connected_players()
 	local out = {}
 	for _,player in ipairs(list) do
 		local name = player:get_player_name()
 		if name then
 			local prefix,color = nick_prefix.get(name)
 			if prefix and color then
-				table.insert(out,core.colorize(color,prefix).." "..name)
+				table.insert(out,minetest.colorize(color,prefix).." "..name)
 			else
 				table.insert(out,name)
 			end
@@ -149,16 +180,16 @@ local say_def = {
 	privs = {shout=true},
 	func = function(name,param)
 		if name and param and param ~= "" then
-			core.run_callbacks(core.registered_on_chat_messages, 5, name, param)
+			minetest.run_callbacks(minetest.registered_on_chat_messages, 5, name, param)
 		end
 	end
 }
 
 
-core.register_on_mods_loaded(function()
-	if core.chatcommands["say"] then
-		core.override_chatcommand("say",say_def)
+minetest.register_on_mods_loaded(function()
+	if minetest.chatcommands["say"] then
+		minetest.override_chatcommand("say",say_def)
 	else
-		core.register_chatcommand("say",say_def)
+		minetest.register_chatcommand("say",say_def)
 	end
 end)
